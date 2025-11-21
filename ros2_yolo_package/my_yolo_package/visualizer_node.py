1d83aa02d72518b9a8d32086afd5b536a7994290
wayneballard <126793814+wayneballard@users.noreply.github.com>
2025-11-21 18:33:10 +0100

    Update visualizer_node.py

--- ros2_yolo_package/my_yolo_package/visualizer_node.py
+++ ros2_yolo_package/my_yolo_package/visualizer_node.py
@@ -1,11 +1,12 @@
-  GNU nano 4.8                                      visualizer_node.py                                                 
 import cv2
 import rclpy 
 import numpy as np
 import threading
+import time
 from rclpy.node import Node
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from vision_msgs.msg import Detection2DArray
 from message_filters import Subscriber, ApproximateTimeSynchronizer
 
 class Visualizer(Node):
@@ -16,30 +17,63 @@ class Visualizer(Node):
         self.get_logger().info("Initializing visualizer node...")
         self.colorMap = cv2.applyColorMap(np.arange(256, dtype=np.uint8), cv2.COLORMAP_JET)
         self.colorMap[0] = [0, 0, 0]
+
+
+        self.x1 = None
+        self.x2 = None
+        self.y1 = None
+        self.y2 = None
+        self.f_x = 457.798
+        self.B = 0.075
+
         self.sub_annotated = self.create_subscription(Image, "annotated_image", self.annotated_callback, 10)
         self.sub_depth =  self.create_subscription(Image, "depth_frame", self.depth_callback, 10)
+        self.sub_detections = self.create_subscription(Detection2DArray, "detections", self.detection_callback, 10)
+
         self.bridge = CvBridge()
 
 
 
-
     def annotated_callback(self, msg: Image):
-        self.annotated_frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-
+        self.annotated_frame = self.bridge.imgmsg_to_cv2(msg, "bgr8") 
 
     def depth_callback(self, msg: Image):
         self.depth_frame = self.bridge.imgmsg_to_cv2(msg, "passthrough")
         self.depth_frame = cv2.resize(self.depth_frame, (640, 480))
         self.depth_frame_colorized = cv2.applyColorMap(self.depth_frame, self.colorMap)
-        
+
+    def detection_callback(self, msg: Detection2DArray):
+        for det in msg.detections:
+            self.x_center = det.bbox.center.x
+            self.y_center = det.bbox.center.y
+            self.size_x  = det.bbox.size_x
+            self.size_y = det.bbox.size_y
+
+            self.x1 = int(self.x_center - self.size_x / 2)
+            self.x2 = int(self.x_center + self.size_x / 2)
+            self.y1 = int(self.y_center - self.size_y / 2)
+            self.y2 = int(self.y_center + self.size_y / 2)
+    
     def visualize(self): 
-        while True:
+        while rclpy.ok():
+            if self.x1 is not None and self.y1 is not None and self.depth_frame is not None:
+                self.region = self.depth_frame[self.y1:self.y2, self.x1:self.x2]
+                self.valid_pixels = self.region[self.region > 0]
+                self.disparity_value = np.mean(self.valid_pixels)
+                if self.disparity_value < 0.1:
+                    self.disparity_value = 0.1
+                self.distance_m = (self.f_x * self.B) / self.disparity_value
+                print(self.distance_m)
+
             if self.annotated_frame is None or self.depth_frame_colorized is None:
+                time.sleep(0.01)
                 continue
             self.combined_streams = np.hstack([self.annotated_frame, self.depth_frame_colorized])
             cv2.imshow("Combined Stream", self.combined_streams)
+
             if cv2.waitKey(1) == ord("q"):
                 break  
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -47,7 +81,8 @@ def main(args=None):
 
     vis_thread = threading.Thread(target=visualizer_node.visualize, daemon=True)
     vis_thread.start()
-
+    
+   
     try:
         rclpy.spin(visualizer_node)
     except(KeyboardInterrupt, SystemExit):
