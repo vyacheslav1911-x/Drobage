cee1ead1a0274f56160b3c58ed4d1476a03fe87c
wayneballard <126793814+wayneballard@users.noreply.github.com>
2025-11-24 12:13:48 +0100

    Update visualizer_node.py

--- ros2_yolo_package/my_yolo_package/visualizer_node.py
+++ ros2_yolo_package/my_yolo_package/visualizer_node.py
@@ -7,6 +7,7 @@ from rclpy.node import Node
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from vision_msgs.msg import Detection2DArray
+from std_msgs.msg import Float32
 from message_filters import Subscriber, ApproximateTimeSynchronizer
 
 class Visualizer(Node):
@@ -30,41 +31,62 @@ class Visualizer(Node):
         self.sub_depth =  self.create_subscription(Image, "depth_frame", self.depth_callback, 10)
         self.sub_detections = self.create_subscription(Detection2DArray, "detections", self.detection_callback, 10)
 
+        self.publisher_frwd_dist = self.create_publisher(Float32, "forward_distance", 10) 
+        self.create_timer(float(1/30), self.ROI_callback)       
+
         self.bridge = CvBridge()
 
 
 
     def annotated_callback(self, msg: Image):
+        msg.header.stamp = self.get_clock().now().to_msg()
+        self.annotated_timestamp = msg.header.stamp
+        self.t_annot = rclpy.time.Time.from_msg(self.annotated_timestamp)  
         self.annotated_frame = self.bridge.imgmsg_to_cv2(msg, "bgr8") 
 
     def depth_callback(self, msg: Image):
+        msg.header.stamp = self.get_clock().now().to_msg()
+        self.depth_timestamp = msg.header.stamp
+        self.t_depth = rclpy.time.Time.from_msg(self.depth_timestamp)
         self.depth_frame = self.bridge.imgmsg_to_cv2(msg, "passthrough")
         self.depth_frame = cv2.resize(self.depth_frame, (640, 480))
         self.depth_frame_colorized = cv2.applyColorMap(self.depth_frame, self.colorMap)
 
     def detection_callback(self, msg: Detection2DArray):
-        for det in msg.detections:
-            self.x_center = det.bbox.center.x
-            self.y_center = det.bbox.center.y
-            self.size_x  = det.bbox.size_x
-            self.size_y = det.bbox.size_y
+        if len(msg.detections) == 0:
+            self.x1 = self.x2 = self.y1 = self.y2 = None
+            return
 
-            self.x1 = int(self.x_center - self.size_x / 2)
-            self.x2 = int(self.x_center + self.size_x / 2)
-            self.y1 = int(self.y_center - self.size_y / 2)
-            self.y2 = int(self.y_center + self.size_y / 2)
+        det =  msg.detections[0]
+        self.x_center = det.bbox.center.x
+        self.y_center = det.bbox.center.y
+        self.size_x  = det.bbox.size_x
+        self.size_y = det.bbox.size_y
+
+        self.x1 = int(self.x_center - self.size_x / 2)
+        self.x2 = int(self.x_center + self.size_x / 2)
+        self.y1 = int(self.y_center - self.size_y / 2)
+        self.y2 = int(self.y_center + self.size_y / 2)
     
-    def visualize(self): 
-        while rclpy.ok():
-            if self.x1 is not None and self.y1 is not None and self.depth_frame is not None:
-                self.region = self.depth_frame[self.y1:self.y2, self.x1:self.x2]
-                self.valid_pixels = self.region[self.region > 0]
-                self.disparity_value = np.mean(self.valid_pixels)
-                if self.disparity_value < 0.1:
-                    self.disparity_value = 0.1
-                self.distance_m = (self.f_x * self.B) / self.disparity_value
-                print(self.distance_m)
+    def ROI_callback(self):
+        self.msg = Float32() 
+        if self.x1 is not None and self.y1 is not None and self.depth_frame is not None:
+            if abs((self.t_depth - self.t_annot).nanoseconds) > 50_000_000:
+                return    
+            self.region = self.depth_frame[self.y1:self.y2, self.x1:self.x2]
+            self.valid_pixels = self.region[self.region > 0]
+            self.disparity_value = np.mean(self.valid_pixels)
+            if self.disparity_value < 0.1:
+                self.disparity_value = 0.1
+            self.distance_m = (self.f_x * self.B) / self.disparity_value
+            print(f"{self.distance_m:.2f}")
+            self.msg.data = round(self.distance_m, 2)
+            self.publisher_frwd_dist.publish(self.msg)
 
+
+
+    def visualize(self):
+        while rclpy.ok():
             if self.annotated_frame is None or self.depth_frame_colorized is None:
                 time.sleep(0.01)
                 continue
@@ -73,7 +95,6 @@ class Visualizer(Node):
 
             if cv2.waitKey(1) == ord("q"):
                 break  
-
 
 def main(args=None):
     rclpy.init(args=args)
@@ -93,6 +114,12 @@ def main(args=None):
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
 
 
 
