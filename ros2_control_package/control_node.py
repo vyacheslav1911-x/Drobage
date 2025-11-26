bd927e79c51659a073524d055fabc8eb5d4a6e6b
wayneballard <126793814+wayneballard@users.noreply.github.com>
2025-11-26 18:11:13 +0100

    Update control_node.py

--- ros2_control_package/control_node.py
+++ ros2_control_package/control_node.py
@@ -14,6 +14,7 @@ class PIController:
             self.integral_error = 0 
             self.last_sample = None
 
+
         def update(self, error):
             self.current_sample = time.monotonic()
             if self.last_sample is None:
@@ -34,20 +35,24 @@ class PIController:
             self.ki_side = ki_side
             self.integral_error_side = 0
             self.last_sample_side = None
+            self.last_error_side = None
 
 
         def update(self, error_side):
             self.current_sample_side = time.monotonic()
             if self.last_sample_side is None:
-                dt = 0.1
+                self.dt = 0.1
             else:
-                dt = max(0.001, self.current_sample_side - self.last_sample_side)
+                self.dt = max(0.001, self.current_sample_side - self.last_sample_side)
             self.last_sample_side = self.current_sample_side
 
-            self.integral_error_side += error_side * dt
-            self.integral_error_side = max(-120, min(120, self.integral_error_side))
-            control_output = self.kp_side * error_side + self.ki_side * self.integral_error_side
-            return max(-255, min(255, control_output))
+            self.integral_error_side += error_side * self.dt            self.integral_error_side = max(-120, min(120, self.integral_error_side))
+            control_output = self.kp_side * error_side + self.ki_side * self.integral_error_side 
+            self.prev_error = self.last_error_side if self.last_error_side is not None else 0
+            self.rate_of_change = (error_side - self.prev_error) / self.dt
+            self.last_error_side = error_side 
+            print(f"Derivative: {self.rate_of_change}")
+            return max(-255, min(255, control_output)), self.rate_of_change
 
 class Control(Node):
     def __init__(self):
@@ -55,15 +60,19 @@ class Control(Node):
         self.Kp_frwd = 1
         self.Ki_frwd = 1
 
-        self.Kp_side = 1
-        self.Ki_side = 1
+        self.Kp_side = 0.7
+        self.Ki_side = 0.5
+        self.TARGET_DISTANCE = 0.2
 
         self.ip = "192.168.4.1"
         self.is_moving = False
+        self.should_turn = True
+        self.should_stop = False
         self.frwd_controller = PIController.Forward(self.Kp_frwd, self.Ki_frwd)
         self.side_controller = PIController.Side(self.Kp_side, self.Ki_side) 
-        self.subscriber_frwd_dist = self.create_subscription(Float32, "forward_distance", self.forward_error_callback, 10)
-        self.subscriber_side_error = self.create_subscription(Int16, "side_error", self.side_error_callback, 10) 
+        self.subscriber_frwd_dist = self.create_subscription(Float32, "forward_distance", self.forward_error_callback, 5)
+        self.subscriber_side_error = self.create_subscription(Int16, "side_error", self.side_error_callback, 5) 
+        self.rate_of_change = None       
 
         self.distance_m = None
         self.side_error = None    
@@ -79,55 +88,85 @@ class Control(Node):
 
 
     def control_loop_frwd(self):
+        should_turn = True
+
         if self.distance_m is None or self.side_error is None:
             return
-        TARGET_DISTANCE = 0.25
-        should_move = self.distance_m > TARGET_DISTANCE
-        control_output_side = self.side_controller.update(self.side_error)      
-        if should_move and control_output_side < 0:
-            error = self.distance_m - TARGET_DISTANCE
-
-            control_output_frwd = self.frwd_controller.update(error)
-
-            speed = max(80, min(255, control_output_frwd))
- 
-            command_move = f'{{"T":11,"L":{int(speed)/1.75},"R":{int(speed)/1.75 + 50*int(abs(control_output_side))}}}'  >
-            json_move = f"http://{self.ip}/js?json={command_move}"
-            try:
-            try:
-                requests.get(json_move, timeout=0.1)
-            except Exception as e:
-                print("HTTP error: ", e)
-            self.is_moving = True
-
-        if should_move and control_output_side > 0:
-            error = self.distance_m - TARGET_DISTANCE
-
-            control_output_frwd = self.frwd_controller.update(error)
-
-            speed = max(80, min(255, control_output_frwd))
- 
-            command_move = f'{{"T":11,"L":{int(speed)+ int(abs(control_output_side))},"R":{int(speed)/1.75}}}'        
-            json_move = f"http://{self.ip}/js?json={command_move}"
-            try:
-                requests.get(json_move, timeout=0.1)
-            except Exception as e:
-                print("HTTP error: ", e)
-            self.is_moving = True
-
-
-        elif not should_move and self.is_moving:
+        should_move = self.distance_m > self.TARGET_DISTANCE
+        control_output_side, self.rate_of_change = self.side_controller.update(self.side_error)
+        if abs(self.side_error) > 10:
+            self.should_stop = False
+        if -10 < self.side_error < 10:
             command_stop = '{"T":11,"L":0,"R":0}'
             json_stop =  f"http://{self.ip}/js?json={command_stop}"
+            self.should_stop = True
             try:
                 requests.get(json_stop, timeout=0.1)
             except Exception as e:
                 print("HTTP error: ", e)
-            self.is_moving = False
-            print("Target reached - stopping the rover")
+        if should_turn and not self.should_stop:    
+            if control_output_side < 0:
+                error = self.distance_m - self.TARGET_DISTANCE
 
-        print(f"{self.distance_m:.2f}")
-        print(control_output_side)
+                control_output_frwd = self.frwd_controller.update(error)
+                speed = max(80, min(255, control_output_frwd))
+ 
+                command_move = f'{{"T":11,"L":{-240},"R":{240}}}'        
+                json_move = f"http://{self.ip}/js?json={command_move}"
+                try:
+                    requests.get(json_move, timeout=0.1)
+                except Exception as e:
+                    print("HTTP error: ", e)
+                self.is_moving = True
+
+            if control_output_side > 0:
+                error = self.distance_m - self.TARGET_DISTANCE
+
+                control_output_frwd = self.frwd_controller.update(error)
+
+                speed = max(80, min(255, control_output_frwd))
+ 
+                command_move = f'{{"T":11,"L":{160},"R":{-160}}}'        
+                json_move = f"http://{self.ip}/js?json={command_move}"
+                try:
+                    requests.get(json_move, timeout=0.1)
+                except Exception as e:
+                    print("HTTP error: ", e)
+                self.is_moving = True
+
+
+#        elif (not should_move and self.is_moving):
+#            command_stop = '{"T":11,"L":0,"R":0}'
+#            json_stop =  f"http://{self.ip}/js?json={command_stop}"
+#            try:
+#                requests.get(json_stop, timeout=0.1)
+#            except Exception as e:
+#                print("HTTP error: ", e)
+#            self.is_moving = False
+#            self.is_stopped = True
+#            print("Target reached - stopping the rover")
+#            if self.is_stopped:
+#                print(self.rate_of_change)
+#                if (-11 < self.rate_of_change < 11) and (-5 < self.side_error < 5):
+#                    return
+#                if self.side_error < 0:
+#                    command_reverse_left = f'{{"T":11,"L":{-150},"R":{150}}}'
+#                    json_reverse_left = f"http://{self.ip}/js?json={command_reverse_left}"
+#                    try:
+#                        requests.get(json_reverse_left, timeout=0.1)
+#                    except Exception as e:
+#                        print("HTTP error: ", e)
+#                elif self.side_error > 0:
+#                    command_reverse_right = f'{{"T":11,"L":{150},"R":{-150}}}'
+#                    json_reverse_right = f"http://{self.ip}/js?json={command_reverse_right}"
+#                    try:
+#                        requests.get(json_reverse_right, timeout=0.1)
+#                    except Exception as e:
+#                        print("HTTP error: ", e)
+  
+
+        print(f"Distance: {self.distance_m:.2f}")
+        print(f"Side control magnitude: {control_output_side}")
 #    def control_loop_side(self):
 #        if self.side_error is None:
 #            return
@@ -171,6 +210,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+            
+
 
 
 
