76eed0e0c045f7bd974116b478696fb62e3f53a1
wayneballard <126793814+wayneballard@users.noreply.github.com>
2026-01-04 20:36:52 +0100

    Update visualizer_node.py

--- ros2_yolo_package/my_yolo_package/visualizer_node.py
+++ ros2_yolo_package/my_yolo_package/visualizer_node.py
@@ -1,4 +1,4 @@
-import cv2
+mport cv2
 import rclpy 
 import numpy as np
 import threading
@@ -10,65 +10,99 @@ from vision_msgs.msg import Detection2DArray
 from std_msgs.msg import Float32, Int16, Bool, Float32MultiArray
 from message_filters import Subscriber, ApproximateTimeSynchronizer
 from filterpy.kalman import KalmanFilter
+#x - state vector
+#z - measurement vector
+#F - state transition matrix
+#H - observation matrix
+#R - measurement noise covariance
+#Q - process noise covariance
+#P - error covariance
 
 class KalmanBox:
-    def __init__(self, bbox):
-        self.kf = KalmanFilter(7, 4)
-        self.kf.F = np.array(
-        [[1,0,0,0,1,0,0], 
-        [0,1,0,0,0,1,0],
-        [0,0,1,0,0,0,1],
-        [0,0,0,1,0,0,0],
-        [0,0,0,0,1,0,0],
-        [0,0,0,0,0,1,0],
-        [0,0,0,0,0,0,1]])
+    class Forward:
+        def __init__(self, bbox):
+            self.kf = KalmanFilter(7, 4)
+            self.kf.F = np.array(
+            [[1,0,0,0,0,0,0], 
+            [0,1,0,0,0,0,0],
+            [0,0,1,0,0,0,0],
+            [0,0,0,1,0,0,0],
+            [0,0,0,0,1,0,0],
+            [0,0,0,0,0,1,0],
+            [0,0,0,0,0,0,1]])
 
-        self.kf.H = np.array(
-        [[1,0,0,0,0,0,0],
-        [0,1,0,0,0,0,0],
-        [0,0,1,0,0,0,0],
-        [0,0,0,1,0,0,0]])
+            self.kf.H = np.array(
+            [[1,0,0,0,0,0,0],
+            [0,1,0,0,0,0,0],
+            [0,0,1,0,0,0,0],
+            [0,0,0,1,0,0,0]]) 
 
-        self.kf.R[:2,:2] *= 10
-        self.kf.Q *= 0.001
-        self.kf.Q[4:,4:] *= 0.00001
-        self.kf.P *= 10
-        self.kf.P[4:, 4:] *= 10
+            self.kf.R[:2,:2] *= 10  
+            self.kf.Q *= 0.001 
+            self.kf.Q[4:,4:] *= 0.00001
+            self.kf.P *= 10 
+            self.kf.P[4:, 4:] *= 10
 
-        self.kf.x[:4] = self.convert_bb_to_z(bbox)
-        self.time_since_det = 0
+            self.kf.x[:4] = self.convert_bb_to_z(bbox)
+            self.time_since_det = 0
 
-    def convert_bb_to_z(self, bbox):
-        w = bbox[2] - bbox[0]
-        h = bbox[3] - bbox[1]
-        cx = bbox[0] + w/2
-        cy = bbox[1] + h/2
-        s = w * h
-        r = w/h
-        z = [cx, cy, s, r]
-        return np.array(z).reshape((4,1))
+        def convert_bb_to_z(self, bbox):
+            w = bbox[2] - bbox[0]
+            h = bbox[3] - bbox[1]
+            cx = bbox[0] + w/2
+            cy = bbox[1] + h/2
+            s = w * h
+            r = w/h
+            z = [cx, cy, s, r]
+            return np.array(z).reshape((4,1))
 
-    def convert_x_to_bb(self, x):
-        w = np.sqrt(x[2] * x[3])
-        h = x[2]/w
-        self.x1_predicted = x[0] - w/2
-        self.y1_predicted = x[1] - h/2
-        self.x2_predicted = x[0] + w/2
-        self.y2_predicted = x[1] + h/2
-        bb_predicted = [self.x1_predicted, self.y1_predicted, self.x2_predicted, self.y2_predicted]
-        return np.array(bb_predicted).reshape((1,4))
+        def convert_x_to_bb(self, x):
+            w = np.sqrt(x[2] * x[3])
+            h = x[2]/w
+            self.x1_predicted = x[0] - w/2
+            self.y1_predicted = x[1] - h/2
+            self.x2_predicted = x[0] + w/2
+            self.y2_predicted = x[1] + h/2
+            bb_predicted = [self.x1_predicted, self.y1_predicted, self.x2_predicted, self.y2_predicted]
+            return np.array(bb_predicted).reshape((1,4))
 
-    def predict(self):
-        self.kf.predict()
-        self.time_since_det += 1
-        return self.get_bbox()
+        def predict(self):
+            self.kf.predict()
+            self.time_since_det += 1
+            return self.get_bbox()
 
-    def update(self, bbox):
-        self.kf.update(self.convert_bb_to_z(bbox))
-        self.time_since_det = 0
+        def update(self, bbox):
+            self.kf.update(self.convert_bb_to_z(bbox))
+            self.time_since_det = 0
 
-    def get_bbox(self):
-        return self.convert_x_to_bb(self.kf.x)[0] 
+        def get_bbox(self):
+            return self.convert_x_to_bb(self.kf.x)[0] 
+
+    class Lateral:
+        def __init__(self, side_error):
+            self.kf = KalmanFilter(2,1)
+            self.kf.F = np.array(
+            [[1,1],
+             [0,1]])
+
+            self.kf.H = np.array(
+            [[1,0]])
+
+            self.kf.x = np.array([[side_error], [0]])
+
+            self.kf.R *= 10
+            self.kf.Q[[0,0],[1,1]] *= 0.00001
+
+        def predict(self):
+            self.kf.predict()
+            return self.get_side_error()
+
+        def update(self, side_error):
+            self.kf.update([[side_error]])
+
+        def get_side_error(self):
+            return self.kf.x[0,0]
+
 
 
 class Visualizer(Node):
@@ -84,6 +118,10 @@ class Visualizer(Node):
         self.image_center = (320, 240)
 
         self.tracker = None
+        self.side_tracker = None
+
+        self.error_predicted = None
+
         self.bbox = None
         self.bbox_predicted = None
         self.bb_center = None    
@@ -109,12 +147,12 @@ class Visualizer(Node):
         self.create_timer(float(1/30), self.side_error_callback)
         self.create_timer(float(1/30), self.detection)
         self.create_timer(float(1/30), self.tracking_loop)
+        self.create_timer(float(1/30), self.side_tracking_loop)
 
         self.bridge = CvBridge()
 
     def annotated_callback(self, msg: Image):
         self.annotated_frame = self.bridge.imgmsg_to_cv2(msg, "bgr8") 
-
     def depth_callback(self, msg: Image):
         self.depth_frame = self.bridge.imgmsg_to_cv2(msg, "passthrough")
         self.depth_frame = cv2.resize(self.depth_frame, (640, 480))
@@ -145,7 +183,7 @@ class Visualizer(Node):
         if self.bbox is None:
             return
         if self.tracker is None and self.bbox is not None:
-            self.tracker = KalmanBox(self.bbox)
+            self.tracker = KalmanBox.Forward(self.bbox)
 
         if self.detected:
             self.tracker.update(self.bbox)
@@ -159,7 +197,7 @@ class Visualizer(Node):
         if not self.detected and self.bbox is None:
             return
         if self.x1 is not None and self.y1 is not None and self.depth_frame is not None and self.detected:
-                      self.region = self.depth_frame[int(self.y1-(self.y1*0.05)):int(self.y2-(self.y2*0.05)), self.x1:self.x2]
+            self.region = self.depth_frame[int(self.y1-(self.y1*0.05)):int(self.y2-(self.y2*0.05)), self.x1:self.x2]
         elif not self.detected and self.bbox_predicted is not None:
             x1, y1, x2, y2, = self.bbox_predicted
             if np.isnan([x1, y1, x2, y2]).any():
@@ -175,14 +213,27 @@ class Visualizer(Node):
         print(msg)
         self.publisher_frwd_dist.publish(msg)
 
+    def side_tracking_loop(self):
+        if self.bbox is None:
+            return
+        if self.side_tracker is None and self.bbox is not None and self.error_x is not None:
+            self.side_tracker = KalmanBox.Lateral(self.error_x)
+        if self.detected:
+            self.side_tracker.update(self.error_x)
+        self.side_tracker.predict()
+        self.error_predicted = self.side_tracker.get_side_error()
 
    
     def side_error_callback(self):
         msg = Int16()
         if self.bb_center is None:
             return
-        self.error_x = int(self.bb_center[0] - self.image_center[0])
+        if self.x1 is not None and self.y1 is not None and self.depth_frame is not None and self.detected:
+            self.error_x = int(self.bb_center[0] - self.image_center[0])
+        elif not self.detected and self.error_predicted is not None:
+            self.error_x = int(self.error_predicted)
         msg.data = self.error_x
+        print(self.error_x)
         self.publisher_err_x.publish(msg)
     
     def detection(self):
@@ -220,6 +271,7 @@ def main(args=None):
 
 if __name__ == "__main__":
     main()
+
 
 
 
