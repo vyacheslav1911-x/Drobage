9b314363f1682958ea2c072ad51128d0bd636adc
wayneballard <126793814+wayneballard@users.noreply.github.com>
2026-03-17 17:28:12 +0100

    Update visualizer_node.py

--- src/my_yolo_package/my_yolo_package/visualizer_node.py
+++ src/my_yolo_package/my_yolo_package/visualizer_node.py
@@ -55,7 +55,7 @@ class KalmanBox:
 
             self.kf.x = np.array([[side_error], [0]])
 
-            self.kf.R *= 10
+            self.kf.R *= 0.05
             self.kf.Q[[0,0],[1,1]] *= 0.00001
 
         def predict(self):
@@ -64,11 +64,11 @@ class KalmanBox:
 
         def update(self, side_error):
             self.kf.update([[side_error]])
-        
+
         def get_side_error(self):
             return self.kf.x[0,0]
 
-         
+
 
 class Visualizer(Node):
     def __init__(self):
@@ -87,21 +87,23 @@ class Visualizer(Node):
 
         self.error_predicted = None
         self.distance_m = None
+        self.distance_predicted = None
+        self.side_error_out = None
 
         self.bbox = None
-        self.bbox_predicted = None
         self.bb_center = None    
         self.x1 = None
         self.x2 = None
         self.y1 = None
         self.y2 = None
         self.detected = False
-        self.measurement_invalid = False
 
         self.disparity_spike = False
         self.prev_distance = None
         self.last_valid_distance = None
-        self.recovery = False 
+        self.last_valid_side_error = None
+        self.recovery = False
+        self.measurement_invalid = False 
 
         self.f_x = 457.798
         self.B = 0.075
@@ -110,23 +112,25 @@ class Visualizer(Node):
         self.sub_annotated = self.create_subscription(Image, "annotated_image", self.annotated_callback, 5)
         self.sub_depth =  self.create_subscription(Image, "depth_frame", self.depth_callback, 5)
         self.sub_detections = self.create_subscription(Detection2DArray, "detections", self.detection_callback, 5)
-        
+
         self.publisher_frwd_dist = self.create_publisher(Float32, "forward_distance", 5) 
         self.publisher_err_x = self.create_publisher(Int16, "side_error", 5)        
         self.publisher_det = self.create_publisher(Bool, "detection", 5)
         self.publisher_bb_coords = self.create_publisher(Float32MultiArray, "coords", 5)
+        self.publisher_meas_invalid = self.create_publisher(Bool, "meas_invalid", 5)
+        self.publisher_recovery = self.create_publisher(Bool, "recovery", 5)
 
-        self.create_timer(float(1/30), self.ROI_callback)       
-        self.create_timer(float(1/30), self.side_error_callback)
-        self.create_timer(float(1/30), self.detection)
-        self.create_timer(float(1/30), self.tracking_loop)
-        self.create_timer(float(1/30), self.side_tracking_loop)
-        
+        self.create_timer(float(1/10), self.ROI_callback)       
+        self.create_timer(float(1/10), self.side_error_callback)
+        self.create_timer(float(1/10), self.detection)
+        self.create_timer(float(1/10), self.tracking_loop)
+        self.create_timer(float(1/10), self.side_tracking_loop)
+
         self.bridge = CvBridge()
-             
+
     def annotated_callback(self, msg: Image):
         self.annotated_frame = self.bridge.imgmsg_to_cv2(msg, "bgr8") 
-        
+
     def depth_callback(self, msg: Image):
         self.depth_frame = self.bridge.imgmsg_to_cv2(msg, "passthrough")
         self.depth_frame = cv2.resize(self.depth_frame, (640, 480))
@@ -141,14 +145,12 @@ class Visualizer(Node):
             self.detected = True
 
         det =  msg.detections[0]
-        self.x_center = det.bbox.center.x
-        self.y_center = det.bbox.center.y
+        self.x_center = det.bbox.center.position.x
+        self.y_center = det.bbox.center.position.y
         self.size_x  = det.bbox.size_x
         self.size_y = det.bbox.size_y
         self.bb_center = (self.x_center, self.y_center)        
 
-
-        
         h, w = 480, 640
 
         self.x1 = max(0, int(self.x_center - self.size_x / 2))
@@ -156,7 +158,6 @@ class Visualizer(Node):
         self.y1 = max(0, int(self.y_center - self.size_y / 2)*0.99)
         self.y2 = min(h, int(self.y_center + self.size_y / 2)*0.99)
         self.bbox = [self.x1, self.y1, self.x2, self.y2]
-
     def is_disparity_spike(self, distance, prev_distance, threshold=0.25):
         if prev_distance is None:
             return False
@@ -164,18 +165,13 @@ class Visualizer(Node):
 
     def recovered(self, actual, last, tol=0.15):
         print(f"DIFFERENCE: {abs(actual - last)}")
-        return abs(actual - last) < tol
+        return bool(abs(actual - last) < tol)
 
-#    def derivative(self):
-#        if self.distance_m is not None:
-#            previous = self.prev_distance if self.prev_distance is not None else 0
-#            self.rate_of_change = (self.distance_m - previous) / 0.033
-#            self.prev_distance = self.distance_m
-#            print(self.rate_of_change)    
-        
 
     def ROI_callback(self):
-        msg = Float32() 
+        msg = Float32()
+        msg_invalid = Bool()
+        msg_recovery = Bool() 
         if not self.detected and self.bbox is None:
             return
         if self.x1 is not None and self.y1 is not None and self.depth_frame is not None and self.detected:
@@ -186,32 +182,42 @@ class Visualizer(Node):
             self.disparity_value = 0.1
         self.distance_m = (self.f_x * self.B) / self.disparity_value  
         self.disparity_spike = self.is_disparity_spike(self.distance_m, self.prev_distance)
-      #  self.prev_distance = self.distance_m
-      #  if self.disparity_spike:
-      #      return
         if self.disparity_spike and not self.measurement_invalid:
             self.measurement_invalid = True
             self.recovery = False
-      #      self.recovery = self.recovered(self.distance_m, self.last_valid_distance)
         if self.measurement_invalid:
             self.recovery = self.recovered(self.distance_m, self.last_valid_distance)     
             if self.recovery:
                 self.measurement_invalid = False
+            if self.distance_predicted is not None and not self.recovery:
+                print("KALMAN CALCULATED")
+                difference = abs(self.distance_predicted - self.last_valid_distance)
+                distance_out = self.last_valid_distance - difference
 
-        if not self.measurement_invalid: 
-            distance_out = self.distance_m
-            self.last_valid_distance = self.distance_m
-        else: 
-            distance_out = self.last_valid_distance
+
+        if not self.measurement_invalid:
+            if self.detected: 
+                print("DETECTED")
+                distance_out = self.distance_m
+                self.last_valid_distance = self.distance_m
+            elif not self.detected:
+                print("ACTUAL KALMAN")
+                distance_out = self.distance_predicted
+
         self.prev_distance = self.distance_m     
-        print(f"{self.distance_m:.2f}")
         print(f"SPIKE: {self.disparity_spike}")
         print(f"MEASUREMENT INVALID: {self.measurement_invalid}")
         print(f"RECOVERY: {self.recovery}")
         print(f"LAST VALID DISTANCE: {self.last_valid_distance}")
-        msg.data = round(distance_out, 2)
+        print(f"DISTANCE: {distance_out}")
+        if not np.isnan(distance_out):
+            msg.data = round(distance_out, 2)
+        msg_invalid.data = self.measurement_invalid
+        msg_recovery.data = self.recovery
         print(msg)
         self.publisher_frwd_dist.publish(msg)
+        self.publisher_meas_invalid.publish(msg_invalid)
+        self.publisher_recovery.publish(msg_recovery)  
  
     def tracking_loop(self):
         if self.distance_m is None:
@@ -225,7 +231,7 @@ class Visualizer(Node):
         print(f"PREDICTED: {self.distance_predicted}")
         print(f"ACTUAL: {self.distance_m}")
 
-           
+
     def side_tracking_loop(self):
         if self.bbox is None:
             return
@@ -235,18 +241,20 @@ class Visualizer(Node):
             self.side_tracker.update(self.error_x)
         self.side_tracker.predict()
         self.error_predicted = self.side_tracker.get_side_error()
+        print(f"SIDE ERROR PREDICTED:{self.error_predicted}")   
 
-   
     def side_error_callback(self):
         msg = Int16()
         if self.bb_center is None:
             return
-        if self.x1 is not None and self.y1 is not None and self.depth_frame is not None and self.detected:
+        if self.x1 is not None and self.y1 is not None and self.depth_frame is not None and self.detected and not self.measurement_invalid:
             self.error_x = int(self.bb_center[0] - self.image_center[0])
-        elif not self.detected and self.error_predicted is not None:
-            self.error_x = int(self.error_predicted)
-        msg.data = max(-320, min(self.error_x, 320))
-        print(self.error_x)
+            self.last_valid_side_error = self.error_x 
+            self.side_error_out = self.error_x
+        elif not self.detected:
+            self.side_error_out = self.last_valid_side_error
+        msg.data = max(-320, min(int(self.side_error_out), 320))
+        print(f"SIDE ERROR OUT:{msg.data}")
         self.publisher_err_x.publish(msg)
     
     def detection(self):
@@ -261,7 +269,7 @@ class Visualizer(Node):
                 continue
             self.combined_streams = np.hstack([self.annotated_frame, self.depth_frame_colorized])
             cv2.imshow("Combined Stream", self.combined_streams)
-     
+
             if cv2.waitKey(1) == ord("q"):
                 break  
 
@@ -284,3 +292,22 @@ def main(args=None):
 
 if __name__ == "__main__":
     main()
+
+
+
+
+    
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
